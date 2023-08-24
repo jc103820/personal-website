d1df03508d2075aa141361ab42ac5981e316e0c9
j <jc103820@ohio.edu>
2023-08-24 19:09:17 -0400

    Add minesweeper board

--- src/app/minesweeper-dialog/minesweeper-dialog.component.ts
+++ src/app/minesweeper-dialog/minesweeper-dialog.component.ts
@@ -7,6 +7,48 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./minesweeper-dialog.component.scss'],
 })
 export class MinesweeperDialogComponent {
-  constructor(public dialogRef: MatDialogRef<MinesweeperDialogComponent>) {}
+  rows: number = 0;
+  columns: number = 0;
+  grid: Cell[][] = [];
 
+  constructor(public dialogRef: MatDialogRef<MinesweeperDialogComponent>) {
+    this.generateGrid();
+  }
+
+  generateGrid(): void {
+    this.columns = 20;
+    this.rows = 10;
+    this.grid = new Array(this.columns);
+
+    for (let i = 0; i < this.columns; i++) {
+      this.grid[i] = new Array(this.rows);
+      for (let j = 0; j < this.rows; j++) {
+        this.grid[i][j] = new Cell();
+      }
+    }
+
+    this.printGrid();
+  }
+
+  printGrid(): void {
+    console.log("Grid:");
+    for (let i = 0; i < this.rows; i++) {
+      let rowString: string = `r${i}   `;
+      for (let j = 0; j < this.columns; j++) {
+        rowString = rowString.concat(` ${this.grid[j][i].content}`);
+      }
+      console.log(rowString);
+    }
+  }
+}
+
+class Cell {
+  content: string = "*";
+  isMine: boolean = false;
+  isFlagged: boolean = false;
+  isRevealed: boolean = false;
+  numBombs: number = 0;
+  color: string = "green";
+
+  constructor() {}
 }
